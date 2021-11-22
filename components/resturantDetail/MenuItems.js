59354a68797eace612f46132e4ecb0b5d3345c16
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-11-22 15:02:02 +0100

    wip loading screen

--- components/resturantDetail/MenuItems.js
+++ components/resturantDetail/MenuItems.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 import { Divider } from "react-native-elements";
 import BouncyCheckBox from "react-native-bouncy-checkbox";
@@ -14,45 +14,15 @@ const styles = StyleSheet.create({
     fontWeight: "500",
   },
 });
-export default function MenuItems({ resturantName }) {
-  const foods = [
-    {
-      food: "Lasagna",
-      desc: "With butter lettuce, tomato and sauce bechamel",
-      price: "$13.50",
-      image:
-        "https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg",
-    },
-    {
-      food: "Tandoori Chicken",
-      desc: "Amazing Indian dish with tenderloin chicken off the sizzles 🔥",
-      price: "$19.20",
-      image: "https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg",
-    },
-    {
-      food: "Chilaquiles",
-      desc: "Chilaquiles with cheese and sauce. A delicious mexican dish 🇲🇽",
-      price: "$14.50",
-      image:
-        "https://i2.wp.com/chilipeppermadness.com/wp-content/uploads/2020/11/Chilaquales-Recipe-Chilaquiles-Rojos-1.jpg",
-    },
-    {
-      food: "Chicken Caesar Salad",
-      desc: "One can never go wrong with a chicken caesar salad. Healthy option with greens and proteins!",
-      price: "$21.50",
-      image:
-        "https://images.themodernproper.com/billowy-turkey/production/posts/2019/Easy-italian-salad-recipe-10.jpg?w=1200&h=1200&q=82&fm=jpg&fit=crop&fp-x=0.5&fp-y=0.5&dm=1614096227&s=c0f63a30cef3334d97f9ecad14be51da",
-    },
-    {
-      food: "pepparmint te",
-      desc: "hot tee with sugar/milk/brown",
-      price: "$3.50",
-      image:
-        "https://media.meds.se/meds/images/maxx-27394300-g-2019-03-08-115604123/455/360/0/yogitea-detox-17-tepasar.jpg",
-    },
-  ];
+export default function MenuItems({
+  resturantName,
+  foods,
+  hideCheckbox,
+  marginLeft,
+}) {
+  
   const dispatch = useDispatch();
-  const [modalVisible, setModalVisible] = useState(false)
+  const [modalVisible, setModalVisible] = useState(false);
 
   const selectItem = (item, checkboxValue) =>
     dispatch({
@@ -68,27 +38,27 @@ export default function MenuItems({ resturantName }) {
   );
 
   const isFoodInCart = (food, cartItems) =>
-    Boolean(
-      cartItems.find((item) => 
-        item.food === food.food
-      )
-    );
-
-    
+    Boolean(cartItems.find((item) => item.food === food.food));
 
   return (
     <>
       {foods.map((food, index) => (
         <View key={index}>
           <View style={styles.menuItemStyles}>
-            <BouncyCheckBox
-              iconStyle={{ borderColor: "black" }}
-              fillColor="#8a2be2"
-              isChecked={isFoodInCart(food, cartItems)}
-              onPress={(checkboxValue) => selectItem(food, checkboxValue)}
-            />
+            {hideCheckbox ? (
+              <></>
+            ) : (
+              <BouncyCheckBox
+                iconStyle={{ borderColor: "black" }}
+                fillColor="#8a2be2"
+                isChecked={isFoodInCart(food, cartItems)}
+                onPress={(checkboxValue) => selectItem(food, checkboxValue)}
+              />
+            )}
             <FoodInfo food={food} />
-            <FoodImage food={food} />
+            <FoodImage food={food} 
+            marginLeft={marginLeft ? margin: 0}
+            />
           </View>
           <Divider
             width={1.2}
@@ -108,11 +78,16 @@ const FoodInfo = (props) => (
     <Text> {props.food.price} </Text>
   </View>
 );
-const FoodImage = (props) => (
+const FoodImage = ({ marginLeft, ...props }) => (
   <View>
     <Image
       source={{ uri: props.food.image }}
-      style={{ width: 100, height: 100, borderRadius: 8 }}
+      style={{
+        width: 100,
+        height: 100,
+        borderRadius: 8,
+        marginLeft: marginLeft
+      }}
     />
   </View>
 );
